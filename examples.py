e0ed34675e4696b46064683f7cf754422656dffe
baunilhamarga <heitor326@hotmail.com>
2026-03-13 19:26:40 +0100

    ✨ refactor examples.py: add example functions for absolute value, sum to n, and GCD

--- examples.py
+++ examples.py
@@ -1,25 +1,42 @@
-from ast_imp import *
+from __future__ import annotations
 
-p = Assign("1", "X", Add(Var("X"), Int(1)))
-res = run(p, {"X": 3})
-print(res.sigma)   # {'X': 4}
-print(res.path)    # ('1',)
+from ast_imp import Add, Assign, Eq, If, Int, Le, Not, Seq, Sub, Var, While, seq
 
-p = If(
-    "1",
-    Le(Var("X"), Int(0)),
-    Assign("2", "Y", Int(0)),
-    Assign("3", "Y", Int(1)),
-)
-print(run(p, {"X": -2, "Y": 99}))
-print(run(p, {"X": 5, "Y": 99}))
-# Expected paths are ('1', '2') and ('1', '3').
 
-p = While(
-    "1",
-    Not(Eq(Var("X"), Int(0))),
-    Assign("2", "X", Sub(Var("X"), Int(1))),
-)
-res = run(p, {"X": 3})
-print(res.sigma)   # {'X': 0}
-print(res.path)    # ('1', '2', '1', '2', '1', '2', '1')
+def ex_abs():
+    # if X <= 0 then X := -X else X := X
+    return If(
+        "1",
+        Le(Var("X"), Int(0)),
+        Assign("2", "X", Sub(Int(0), Var("X"))),
+        Assign("3", "X", Var("X")),
+    )
+
+
+def ex_sum_to_n():
+    # Y := 0; while X != 0 do (Y := Y + X; X := X - 1)
+    return seq(
+        Assign("1", "Y", Int(0)),
+        While(
+            "2",
+            Not(Eq(Var("X"), Int(0))),
+            seq(
+                Assign("3", "Y", Add(Var("Y"), Var("X"))),
+                Assign("4", "X", Sub(Var("X"), Int(1))),
+            ),
+        ),
+    )
+
+
+def ex_gcd_subtractive():
+    # while X != Y do if X <= Y then Y := Y - X else X := X - Y
+    return While(
+        "1",
+        Not(Eq(Var("X"), Var("Y"))),
+        If(
+            "2",
+            Le(Var("X"), Var("Y")),
+            Assign("3", "Y", Sub(Var("Y"), Var("X"))),
+            Assign("4", "X", Sub(Var("X"), Var("Y"))),
+        ),
+    )
